8548a0516280d7111331300a23e3e45c25209d26
ᛃ Exéctrøn. <Zewsta0@gmail.com>
2024-12-23 18:40:47 +0300

    Create Ask.py

---
+++ Ask.py
@@ -0,0 +1,8 @@
+#
+# Telif Hakkı (C) 2020-2025, TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# Bu dosya < https://github.com/TeamYukki/YukkiMusicBot > projenin bir parçasıdır,
+# ve "GNU v3.0 Lisans Sözleşmesi" altında yayınlanmıştır.
+# Lütfen bakınız < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# Tüm hakları saklıdır.
